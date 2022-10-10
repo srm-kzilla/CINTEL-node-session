0c16689124197e88bb3705f2952d5d5a1d45b6af
Kevin-Aaaquil <eshansingh2409@gmail.com>
2022-10-11 02:10:50 +0530

    add c++ code to compare

--- variables.js
+++ variables.js
@@ -1,7 +1,7 @@
 const constData = "hey";
 
 //cannot be changed, will throw an error
-// data = "hi";
+// constData = "hi";
 console.log(constData);
 
 let letData = "hi";
@@ -24,3 +24,17 @@ console.log(varData);
 var varData = "ok";
 // let letData = "not ok";
 
+
+// As compared to C++ code;
+/**
+ * C++ CODE
+ * #include<iostream>
+ * using namespace std;
+ * int main(){
+ * int a = 10;
+ * char b = 'a';
+ * cout<<a<<endl;           // OUTPUT: 10  [type: int]
+ * cout<<b<<endl;           // OUTPUT: a  [type: char]
+ * return 0;
+ * }
+ */
